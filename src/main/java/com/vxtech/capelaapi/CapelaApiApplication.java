dac6992dbb2ea10ee8781f444fd97f9a995bc5d8
VIctor Ximenis <victor.ximenis@smilego.com.br>
2022-01-21 22:12:33 -0300

    Primeiro commit

---
+++ src/main/java/com/vxtech/capelaapi/CapelaApiApplication.java
@@ -0,0 +1,13 @@
+package com.vxtech.capelaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapelaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapelaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vxtech/capelaapi/CapelaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vxtech.capelaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapelaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
