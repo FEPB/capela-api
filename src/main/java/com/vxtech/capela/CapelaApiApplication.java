b72b8e0e095f740a914b035f12776bb48c4e7879
VIctor Ximenis <victor.ximenis@smilego.com.br>
2022-01-21 23:20:23 -0300

    refatorando pacotes

--- src/main/java/com/vxtech/capela/CapelaApiApplication.java
+++ src/main/java/com/vxtech/capela/CapelaApiApplication.java
@@ -1,4 +1,4 @@
-package com.vxtech.capelaapi;
+package com.vxtech.capela;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/vxtech/capela/CapelaApiApplicationTests.java
+++ src/test/java/com/vxtech/capela/CapelaApiApplicationTests.java
@@ -1,4 +1,4 @@
-package com.vxtech.capelaapi;
+package com.vxtech.capela;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
